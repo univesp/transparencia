adaa314e2b15768d2e1c7a6923c02b155ff18ae9
Evandro Almeida <89evandroalmeida@gmail.com>
2020-10-16 14:23:19 -0300

    Gerando primeiro XML completo da Folha Ordinária

--- lib/audesp_core.rb
+++ lib/audesp_core.rb
@@ -1,3 +1,5 @@
+MUNICIPIO = 7107.freeze
+ENTIDADE = 11219.freeze
 ROOT_PATH = File.expand_path('../files/templates_audesp',
                              __FILE__)
                 .gsub('/lib','')
@@ -15,6 +17,59 @@ def run_xml_folha_ordinaria(worksheet)
         File.join(ROOT_PATH, 'verbas.txt')
     )
 
+    financial_list = []
+    financial_data_sheet = worksheet.sheets[3]
+    financial_data_sheet.rows.each_with_index do |row, index|
+      person_code = row[9].to_i
+
+
+      launches = [] # controla cada lançamento financeiro
+
+      index = 11
+      (0..30).each do
+        ## Código da verba
+        code = row[index].to_s.to_i
+        break if code == 0 # se o código não existe não há mais nenhum
+        # lançamento daqui pra frente
+
+        ## Valor
+        value = ((row[index + 1].to_f).fdiv(100)).to_s
+
+        ## Espécie
+        # AUDESP: 1 - Desconto,2 - Vencimento
+        # Portal: D - Débito, C - Crédito ao funcionário
+        #         R - Redutor Salarial / Reposição (não utilizado),
+        #         S - Reposição de Desconto (não utilizado)
+        kind = row[index + 2].to_s.split('-')[0].to_s.strip
+        kind = kind == 'C' ? 2 : 1
+
+        ## Natureza
+        # AUDESP: 1 - Atrasado, 2 - Normal
+        # Portal: M - Do Mês, A - Atrasado
+        nature = row[index + 3].to_s.split('-')[0].to_s.strip
+        nature = nature == 'A' ? 1 : 2
+
+        ## Código do tipo da verba
+        type_code = row[index + 5].to_s.split('-')[0].to_s.strip.to_i
+
+        index += 8 # incremento de 8 para pular para o próximo bloco de
+        # lançamentos
+
+        launches.push({
+          code: code,
+          value: value,
+          nature: nature,
+          kind: kind,
+          type_code: type_code
+        })
+      end
+
+      financial_list.push({
+                              person_code: person_code,
+                              launches: launches
+                          })
+    end
+
 
     items = ''
     personal_data_sheet = worksheet.sheets[2]
@@ -22,8 +77,21 @@ def run_xml_folha_ordinaria(worksheet)
       next if index == 0 # pula o cabeçalho
       break if row[2].to_s.empty? # evita as linhas em branco
 
-      puts "row[151].to_s"
-      puts row.inspect
+      person_financial_data = financial_list.select { |f|
+        f[:person_code] == row[9].to_i
+      }
+
+      remuneration = ''
+      person_financial_data.first[:launches].each do |launch|
+        remuneration += template_remuneration
+                          .gsub('[MUNICIPIO]',MUNICIPIO.to_s)
+                          .gsub('[ENTIDADE]',ENTIDADE.to_s)
+                          .gsub('[CODIGO_VERBA]', launch[:code].to_s)
+                          .gsub('[VALOR]', launch[:value].to_s)
+                          .gsub('[NATUREZA]', launch[:nature].to_s)
+                          .gsub('[ESPECIE]', launch[:kind].to_s)
+                          .gsub('[CODIGO_TIPO_VERBA]', launch[:type_code].to_s)
+      end if person_financial_data.size > 0
 
       items += template_agent
                  .gsub('[CPF]',"#{row[24].to_s.strip}#{row[25].to_s.strip}")
@@ -57,9 +125,8 @@ def run_xml_folha_ordinaria(worksheet)
                  .gsub('[DESCONTOS]',"#{((row[132].to_f).fdiv(100)).to_s}")
                  .gsub('[REMUNERACAO_LIQUIDA]',"#{((row[133].to_f).fdiv(100))
                                                       .to_s}")
-                 .gsub('[VERBAS]','')
+                 .gsub('[VERBAS]',remuneration)
 
-      break
 
     end
 
